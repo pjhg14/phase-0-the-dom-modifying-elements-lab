97a54ea96518eca2d2c8e8b9468a156fcf6babea
pj <pjhg14@gmail.com>
2021-03-08 19:30:27 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const body = document.body
+body.removeChild(body.querySelector("main"));
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', "victory");
+newHeader.innerHTML = "Paul is the champion";
+
+//body.appendChild(newHeader);
